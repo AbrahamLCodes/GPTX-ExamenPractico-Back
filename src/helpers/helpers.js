660a04de8937015e2c9d4af4eba5686d9b9089f3
Abraham Luna <abraham11999@hotmail.com>
2022-01-20 14:32:55 -0700

    Configuracion de Headers

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,13 +1,11 @@
 module.exports = {
     hasAllKeys: (keys, object) => {
         let has = true;
-        console.log("BODDY asdasd", object);
         keys.forEach(key => {
             if (!Object.hasOwnProperty.bind(object)(key)) {
                 has = false;
             }
         });
-        console.log("HAS ALL KEYS?", has);
         return has;
     }
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ const port = 3000;
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
+
+app.all("/*", (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, X-Requested-With');
+  next();
+});
+
 
 // Rutas API Persona
 require('./personas/routes')(app);
